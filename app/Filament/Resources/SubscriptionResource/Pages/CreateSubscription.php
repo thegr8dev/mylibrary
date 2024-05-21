7b84ddd9da5769dabc4920b536397485099f6e95
thegr8dev <thegr8dev@users.noreply.github.com>
2024-05-21 12:01:32 +0000

    Add copyright headers

--- app/Filament/Resources/SubscriptionResource/Pages/CreateSubscription.php
+++ app/Filament/Resources/SubscriptionResource/Pages/CreateSubscription.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ - Copyright (c) 2024 @thegr8dev
+ -
+ - This source code is licensed under the MIT license found in the
+ - LICENSE file in the root directory of this source tree.
+ -
+ - Made in India.
+ */
+
 namespace App\Filament\Resources\SubscriptionResource\Pages;
 
 use App\Filament\Resources\SubscriptionResource;
